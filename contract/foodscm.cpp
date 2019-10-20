c875a124e8ece13bb040cd62550848d724304afd
Utsav Talwar <talwar.utsav@gmail.com>
2019-10-20 23:48:59 +0530

    Update foodscm.cpp

--- contract/foodscm.cpp
+++ contract/foodscm.cpp
@@ -1,3 +1,4 @@
+#include <eosio/asset.hpp>
 #include <eosio/eosio.hpp>
 
 using namespace std;
@@ -7,24 +8,27 @@ class [[eosio::contract]] foodscm : public eosio::contract {
   private:
 
     struct [[eosio::table]] cropdetails {
-        name customer;
+        uint64_t productId;
+        name producer;
+        name buyer;
         string imageHash;
         string cropName;
         uint64_t cropAmount;
+        uint64_t price;
+        bool sold;
         
-        auto primary_key()const { return customer.value;}
+        auto primary_key()const { return productId;}
     };
     
-    struct [[eosio::table]] userdata 
-    {
+    struct [[eosio::table]] userdata {
       name customer;
       string accType;
       uint64_t tokenBalance;
-      
+
       auto primary_key()const { return customer.value;}
     };
     
-    typedef eosio::multi_index<name("cropdetails"), cropdetails> crop_data;
+    typedef eosio::multi_index<name("cropsdetail"), cropdetails> crop_data;
     typedef eosio::multi_index<name("userdata"), userdata> user_data;  
   
     crop_data _cropdata;
@@ -33,8 +37,7 @@ class [[eosio::contract]] foodscm : public eosio::contract {
   public:
     using contract::contract;
 
-    foodscm( name receiver, name code, datastream<const char*> ds ):
-                                                  contract(receiver, code, ds),
+    foodscm( name receiver, name code, datastream<const char*> ds ): contract(receiver, code, ds),
                        _cropdata(receiver, receiver.value),
                        _userdata(receiver, receiver.value){}
     
@@ -49,22 +52,69 @@ class [[eosio::contract]] foodscm : public eosio::contract {
     }
                        
     [[eosio::action]]
-    void uploadcrop(name customer, string cropName, uint64_t cropAmount, 
-                                                            string imageHash) {
-        _cropdata.emplace(_self, [&](auto& row) {
-            row.customer = customer;
-            row.cropName = cropName;
-            row.cropAmount = cropAmount;
-            row.imageHash = imageHash;
-        }); 
+    void uploadcrop(name producer, string cropName, uint64_t cropAmount, string imageHash, uint64_t price) {
+
+      _cropdata.emplace(_self, [&](auto& row) {
+          row.productId = _cropdata.available_primary_key();
+          row.producer = producer;
+          row.cropName = cropName;
+          row.cropAmount = cropAmount;
+          row.imageHash = imageHash;
+          row.price = price;
+          row.sold = false;
+          row.buyer = producer;
+      }); 
     }
     
     [[eosio::action]]
-    void buycrop(name farmer, name buyer) {
-      //Transfer tokens
-      //Transfer volume
-    }
-};
+    void buycrop(name buyer, uint64_t cropPid, asset quantity, string memo) 
+    {
+        auto itr = _cropdata.find(cropPid);
+        name seller = itr->producer;
 
+        struct transfer
+        {
+            name buyer;
+            name seller;
+            asset quantity;
+            string memo;
+        };
+
+        eosio::action transfer_action = eosio::action(
+            permission_level(_self, name("active")),
+            name("eosio.token"), 
+            name("transfer"),
+            transfer{buyer, seller, quantity, memo});
+            transfer_action.send();
+        
+        auto payer = has_auth( seller ) ? seller : buyer;
+        _cropdata.modify(itr, payer, [&](auto& row){
+          row.sold = true;
+          row.buyer = buyer;
+        });
+    }
+   /* void buycrop(name buyer, uint64_t cropPid) {
+      
+      auto itr = _cropdata.find(cropPid);
+      name seller = itr->producer;
+      uint64_t cropAmount = itr->cropAmount;
+      
+      //transfer tokens to cropPid.customer 
+      check( buyer != seller, "cannot transfer to self" );
+      require_auth( buyer );
+      check( is_account( seller ), "to account does not exist");
+      
+      require_recipient( buyer );
+      require_recipient( seller );
+          
+      auto payer = has_auth( seller ) ? seller : buyer;
+
+    //  sub_balance( buyer, cropAmount );
+    //  add_balance( seller, cropAmount, payer );
+      
+        
+      }*/
+
+};
 
 
