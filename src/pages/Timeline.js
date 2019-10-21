ed25055a0508dc6918416d2680cf595beb49580d
Jai Kapur <jaikapur98@gmail.com>
2019-10-21 17:14:28 +0530

    Track order UI done

---
+++ src/pages/Timeline.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import OrderTimeline from '../components/OrderTimeline/OrderTimeline';
+
+function Timeline(){
+    return(
+        <div className="Timeline-container">
+            <OrderTimeline />
+        </div>
+    )
+}
+
+export default Timeline;
