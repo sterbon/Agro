933517ea4dbc74b94acd09b9cb8972313e4cb3fd
Jitender Verma <vermajounty1999@gmail.com>
2019-10-24 00:08:05 +0530

    Crop Tracking

--- src/pages/OrderTimelinePage.js
+++ src/pages/OrderTimelinePage.js
@@ -54,10 +54,7 @@ class OrderTimelinePage extends Component {
         const { cropId, cropDetails } = this.state;
         // console.log("Crop Detail: ", cropDetails);
         let timeline = null;
-        if(cropId === "") {
-            timeline = <p className="error-timeline">Enter valid crop ID!</p>
-        }
-        else if(cropDetails && (cropDetails.productId == cropId)) {
+        if(cropDetails) {
             timeline = <Timeline cropDetails={cropDetails}/>;
         }
         else if(cropDetails === undefined) {
