820dbb744c9a43eecdeddbcd8a21f1c14a06c0c2
Sterbon <talwar.utsav@gmail.com>
2020-03-19 14:20:57 +0530

    Added features

--- src/scatter/localWallet_helper.js
+++ src/scatter/localWallet_helper.js
@@ -1,8 +1,10 @@
 import { Api, JsonRpc, RpcError } from 'eosjs';
 import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig'
 import ecc from 'eosjs-ecc'
+import createHash from 'create-hash';
 
-const ipfsClient = require('ipfs')
+const sha512 = data => createHash('sha512').update(data).digest('hex');
+
 const nodeFetch = require('node-fetch');
 
 const network = {
@@ -17,8 +19,6 @@ const network = {
     verbose: false,
     sign: true
 };
-// const eos = Eos(network);
-
 
 // Retrieving multi-index data
 export async function getCropDetailsTable() {
@@ -38,132 +38,187 @@ export async function getCropDetailsTable() {
     }
 }
 
-export async function getAccount() {
+export async function getDetailsByCropId (cropId){
+    try {
+        const rpc = new JsonRpc('https://jungle2.cryptolions.io:443', { nodeFetch });
+
+        const result = await rpc.get_table_rows({
+            "json": true,
+            "code": "sterbon23451",
+            "scope": "sterbon23451",
+            "table": "crpdetail",
+            "limit": 1,
+            "lower_bound": cropId,
+        });
+        return result
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function getTransactionDetails () {
+    try {
+        const rpc = new JsonRpc('https://jungle2.cryptolions.io:443', { nodeFetch });
+        const result = await rpc.get_table_rows({
+            "json": true,
+            "code": "sterbon23451",
+            "scope": "sterbon23451",
+            "table": "trxdetail",
+        });
+        return result
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export async function getAccount(account_name) {
     try {
         const rpc = new JsonRpc('https://jungle2.cryptolions.io:443', { nodeFetch });
         // const result = await rpc.get_account('hellokittu15');
-        const result = await rpc.get_account('sterbon23451')
-        return (result.account_name)
-        
+        const result = await rpc.get_account(account_name)
+        if (result.account_name)
+            return true
+
     } catch (e) {
         console.log(e);
         if (e instanceof RpcError)
-          console.log(JSON.stringify(e.json, null, 2).details[0].message);
-      }
+            // console.log(JSON.stringify(e.json, null, 2));
+            return false
+    }
 }
 
-//IPFS for generating private and public keys
-// https://ipfs.io/ipfs/QmW4XxaEg8cWsYisfjnjqLFi1MbHMYjt7nbCh8ZHwgg9c2
-export async function generateKeys() {
-
+//Generating private and public keys
+export const generateKeys = () => {
     ecc.randomKey().then(x => {
         var private_key = x;
         var public_key = ecc.privateToPublic(private_key);
         console.log(private_key, public_key)
+        return (private_key, public_key)
     })
 }
 
 // Create new account on jungle Testnet
 export const createNewAccount = async (account_name, owner_publicKey, active_publicKey) => {
-    account_name = 'hellokittu14';
-    owner_publicKey = 'EOS66RWJB8EGP2gNXEUJ4BoFA7n5DNeQdcBrbjpQTWywGYjFFgTXd';
-    active_publicKey = 'EOS66RWJB8EGP2gNXEUJ4BoFA7n5DNeQdcBrbjpQTWywGYjFFgTXd';
+    const { private_key, public_key } = generateKeys()
+
+    owner_publicKey = public_key
+    active_publicKey = public_key
 
     const signatureProvider = new JsSignatureProvider(['5JdPutdYAYWcjZFsYudKMuLUY8xtnvSBvFg8Cgnbdaxg5rC3h2v']);
     const rpc = new JsonRpc('https://jungle2.cryptolions.io:443', { nodeFetch });
     const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
-    try {
-        const result = await api.transact(
-            {
-                actions: [
-                    {
-                        account: 'eosio',
-                        name: 'newaccount',
-                        authorization: [
-                            {
-                                actor: 'sterbon23451',
-                                permission: 'active',
-                            },
-                        ],
-                        data: {
-                            creator: 'sterbon23451',
-                            name: account_name,
-                            owner: {
-                                threshold: 1,
-                                keys: [
-                                    {
-                                        key: owner_publicKey,
-                                        weight: 1,
-                                    },
-                                ],
-                                accounts: [],
-                                waits: [],
-                            },
-                            active: {
-                                threshold: 1,
-                                keys: [
-                                    {
-                                        key: active_publicKey,
-                                        weight: 1,
-                                    },
-                                ],
-                                accounts: [],
-                                waits: [],
+
+    if (getAccount(account_name)) {
+        try {
+            const result = await api.transact(
+                {
+                    actions: [
+                        {
+                            account: 'eosio',
+                            name: 'newaccount',
+                            authorization: [
+                                {
+                                    actor: 'sterbon23451',
+                                    permission: 'active',
+                                },
+                            ],
+                            data: {
+                                creator: 'sterbon23451',
+                                name: account_name,
+                                owner: {
+                                    threshold: 1,
+                                    keys: [
+                                        {
+                                            key: owner_publicKey,
+                                            weight: 1,
+                                        },
+                                    ],
+                                    accounts: [],
+                                    waits: [],
+                                },
+                                active: {
+                                    threshold: 1,
+                                    keys: [
+                                        {
+                                            key: active_publicKey,
+                                            weight: 1,
+                                        },
+                                    ],
+                                    accounts: [],
+                                    waits: [],
+                                },
                             },
                         },
-                    },
-                    {
-                        account: 'eosio',
-                        name: 'buyrambytes',
-                        authorization: [
-                            {
-                                actor: 'sterbon23451',
-                                permission: 'active',
+                        {
+                            account: 'eosio',
+                            name: 'buyrambytes',
+                            authorization: [
+                                {
+                                    actor: 'sterbon23451',
+                                    permission: 'active',
+                                },
+                            ],
+                            data: {
+                                payer: 'sterbon23451',
+                                receiver: account_name,
+                                bytes: 8192,
                             },
-                        ],
-                        data: {
-                            payer: 'sterbon23451',
-                            receiver: account_name,
-                            bytes: 8192,
                         },
-                    },
-                    {
-                        account: 'eosio',
-                        name: 'delegatebw',
-                        authorization: [
-                            {
-                                actor: 'sterbon23451',
-                                permission: 'active',
+                        {
+                            account: 'eosio',
+                            name: 'delegatebw',
+                            authorization: [
+                                {
+                                    actor: 'sterbon23451',
+                                    permission: 'active',
+                                },
+                            ],
+                            data: {
+                                from: 'sterbon23451',
+                                receiver: account_name,
+                                stake_net_quantity: '1.0000 EOS',
+                                stake_cpu_quantity: '1.0000 EOS',
+                                transfer: false,
                             },
-                        ],
-                        data: {
-                            from: 'sterbon23451',
-                            receiver: account_name,
-                            stake_net_quantity: '1.0000 EOS',
-                            stake_cpu_quantity: '1.0000 EOS',
-                            transfer: false,
                         },
-                    },
-                ],
-            },
-            {
-                blocksBehind: 3,
-                expireSeconds: 30,
-            }
-        );
-        console.log('transaction_id is : ', result.transaction_id);
-        return result.transaction_id;
-    } catch (err) {
-        console.log('error is : ___', err);
+                    ],
+                },
+                {
+                    blocksBehind: 3,
+                    expireSeconds: 30,
+                }
+            );
+            console.log('transaction_id is : ', result.transaction_id);
+            // storeKeys(private_key, account_name, password)
+            return result.transaction_id;
+        } catch (err) {
+            console.log('error is : ___', err);
+        }
     }
+
+    else
+        return ('Account cannot be created')
+
 };
 
 // Storing pvt keys to local storage 
-export const storeKeys = (pvtKey, uname) => {
+export const storeKeys = (pvtKey, uname, password) => {
 
-    localStorage.setItem("privateKey", pvtKey)
+    localStorage.setItem("password", sha512(password))
+    localStorage.setItem("privateKey", ecc.Aes.encrypt(password, pvtKey))
     localStorage.setItem("username", uname)
-    console.log(localStorage.getItem("privateKey"))
+    // ecc.Aes.encrypt(password, pvtKey)
+
+    console.log("key stored as : ", ecc.Aes.encrypt(password, pvtKey))
+}
+
+export const login = (password) => {
+    let passHash = localStorage.getItem("password")
+    let pvtHash = localStorage.getItem("privateKey")
+    if (sha512(password) == passHash) {
+        return ecc.Aes.decrypt(passHash, pvtHash)
+    }
 }
 
 // Signing transactions
