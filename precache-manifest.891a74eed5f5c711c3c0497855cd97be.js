88f3ba6c0a21327374a6c7d6cad4dc418324b4fa
Sterbon <talwar.utsav@gmail.com>
2020-04-02 21:57:02 +0530

    Updates

---
+++ precache-manifest.891a74eed5f5c711c3c0497855cd97be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ea7e314bc9d9d7f43842c6160bf25c",
+    "url": "/Agro/index.html"
+  },
+  {
+    "revision": "0a1c706712ed57d86d4d",
+    "url": "/Agro/static/css/main.0e7dcf87.chunk.css"
+  },
+  {
+    "revision": "b2bfaa785b24baa69d75",
+    "url": "/Agro/static/js/2.13c52699.chunk.js"
+  },
+  {
+    "revision": "a9cbcca921a8d4317e9dc7f2bd781fdb",
+    "url": "/Agro/static/js/2.13c52699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a1c706712ed57d86d4d",
+    "url": "/Agro/static/js/main.42018892.chunk.js"
+  },
+  {
+    "revision": "060393e8b266c24a0cf2",
+    "url": "/Agro/static/js/runtime-main.c723f311.js"
+  },
+  {
+    "revision": "4a40f39c87b10e633d9b506c8d6e41b6",
+    "url": "/Agro/static/media/Grain.4a40f39c.jpg"
+  },
+  {
+    "revision": "71ffaa02b8a349b4a941bf1b6f3f60b0",
+    "url": "/Agro/static/media/addProduct.71ffaa02.png"
+  },
+  {
+    "revision": "4a633ef101cc1482d94242c82291531e",
+    "url": "/Agro/static/media/buy.4a633ef1.png"
+  },
+  {
+    "revision": "82fa206b257a54a950c795c83ad5fcf8",
+    "url": "/Agro/static/media/explore.82fa206b.png"
+  },
+  {
+    "revision": "01750d7ce06ab7678b883c90e4974287",
+    "url": "/Agro/static/media/farmer.01750d7c.png"
+  },
+  {
+    "revision": "6002450e78b69fff7f4da4c1f030ae1f",
+    "url": "/Agro/static/media/penny.6002450e.png"
+  },
+  {
+    "revision": "184b7313e46a4c4ee6195486f97addfb",
+    "url": "/Agro/static/media/upload.184b7313.png"
+  }
+]);
