67b899fa1f56519929d094599f162cea23e5a3fc
Jitender Verma <vermajounty1999@gmail.com>
2020-04-11 13:09:00 +0530

    Order Card Image

--- src/pages/OrdersPage.js
+++ src/pages/OrdersPage.js
@@ -1,12 +1,35 @@
 import { Segment, Header, Button } from "semantic-ui-react";
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import penny from '../static/images/penny.png';
 import { getTransactionDetails } from '../scatter/localWallet_helper';
 import './OrdersPage.css';
+import Unsplash, { toJson } from 'unsplash-js';
+
+const KEY = "e7d3d4ff6d6694a54dc10ca8ddb7e32b8033a64b2e8971cbc27ac49ab2395f5e";
 
 class OrderCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cropImage: penny,
+        }
+        // console.log("Constructor: ", props);
+        const cropName = props.transaction.cropName.trim();
+        const unsplash = new Unsplash({ 
+            accessKey: KEY 
+        });
+        const query = `${cropName}`;
+        unsplash.search.photos(query, 1, 1, { orientation: "landscape" })
+        .then(toJson)
+        .then(result => {
+            if(result.results[0].urls.regular) {
+                // cropImage = result.results[0].urls.raw;
+                this.setState({ cropImage : result.results[0].urls.regular });
+            }
+        });
+    }
+
     render() {
         const { account, transaction } = this.props;
         const { transactionID, cropName, productId, farmer, buyer, price, quantity } = transaction;
@@ -42,7 +65,7 @@ class OrderCard extends Component {
                         </div>
 
                         <div className="order-img-container">
-                            <img src={penny} width="120px" height="120px" />
+                            <img src={this.state.cropImage} width="120px" height="120px" />
                             <div id="orderProduct-details" >
                                 <h4 id="pprice" >Crop ID : <span>{`${"G36C"}${productId}`}</span></h4>
                                 <h4 id="pprice" >Quantity : <span>{quantity} kg</span></h4>
@@ -85,9 +108,9 @@ class OrdersPage extends Component {
     getTransactionDetailFunc() {
         const transactionList = [];
         const { loggedIn, currentUser } = this.state;
-        console.log("loggedIn in Func: ", loggedIn);
+        // console.log("loggedIn in Func: ", loggedIn);
         if (loggedIn) {
-            console.log("here gTD");
+            // console.log("here gTD");
             getTransactionDetails()
                 .then((result) => {
                     const transactions = result.rows;
@@ -107,7 +130,7 @@ class OrdersPage extends Component {
         const { loggedIn, currentUser } = this.state;
         const { transactionList } = this.state;
         console.log("translist:", transactionList);
-        console.log("loggedIn", loggedIn);
+        // console.log("loggedIn", loggedIn);
         let ListView = <p className="else-text">No Orders Placed Yet.</p> ;
         if (transactionList.length) {
             ListView = Object.values(transactionList).map((transaction) => {
