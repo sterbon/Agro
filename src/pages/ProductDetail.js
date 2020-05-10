db76ec372b5c19c3d657a3429358163dea7aca30
Utsav Talwar <talwar.utsav@gmail.com>
2020-05-10 10:29:35 +0530

    Update ProductDetail.js

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -23,13 +23,13 @@ class ProductDetail extends Component {
     buyData(e) {
         e.preventDefault();
         console.log("Prod: ", this.state.selectedProducer.productId)
-        // this.TransactionModal()
-        // buyCrop(this.state.selectedProducer.productId)
-        // .then((result) =>
-        //     {
-        //         console.log(result)
-        //         this.setState({ productBought: true });
-        //     });
+        this.TransactionModal()
+        buyCrop(this.state.selectedProducer.productId)
+        .then((result) =>
+            {
+                console.log(result)
+                this.setState({ productBought: true });
+            });
     }
 
 
